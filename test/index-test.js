eadabcfe9935823813049348df610fe5aa90e247
Hamza Hedfi <h.hedfi@outlook.com>
2018-10-02 11:48:27 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,8 @@
 var index = require("../index.js")
 
 /*
-describe('what-is-a-test', () => {describe('Name', () => {
+describe('what-is-a-test', () => {
+  describe('Name', () => {
     it('returns "Susan"', () => {
       expect(index.name).toEqual('Susan')
     })
